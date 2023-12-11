7733d15a96a7c8a002c80925dfc3d4ae499c998d
bernd-roth <berndroth0@gmail.com>
2023-12-11 22:26:44 +0100

    MainActivity: simple representation of gameboard implemented; two list of figures (red/blue) shuffled

--- app/src/main/java/at/co/netconsulting/MainActivity.kt
+++ app/src/main/java/at/co/netconsulting/MainActivity.kt
@@ -1,6 +1,7 @@
 package at.co.netconsulting
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -22,12 +23,14 @@ import at.co.netconsulting.figures.Scout
 import at.co.netconsulting.figures.Sergeant
 import at.co.netconsulting.figures.Spy
 import at.co.netconsulting.general.StaticFields
+import java.util.Collections
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val figuresOnBoard = ArrayList<Figure>()
-    var gameBoard = Array(10) {Array(10) {0} }
+    private var redFiguresOnBoard: ArrayList<Figure> = arrayListOf()
+    private var blueFiguresOnBoard: ArrayList<Figure> = arrayListOf()
+    private var gameBoard = Array(10) {Array(10) {0} }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,42 +50,162 @@ class MainActivity : AppCompatActivity() {
 
         //generate figures
         initalizePieces()
+        //setup game board with figures randomly placed
         setupGameBoard()
     }
 
     private fun setupGameBoard() {
-        for(figure in figuresOnBoard) {
-            //gameBoard.set()
+        shufflePieces(redFiguresOnBoard)
+        shufflePieces(blueFiguresOnBoard)
+
+        placeFiguresOnGameBoard(StaticFields.RED)
+        placeFiguresOnGameBoard(StaticFields.BLUE)
+
+        printGameBoard()
+    }
+
+    private fun printGameBoard() {
+        var mString:String? = ""
+
+        for(i in 0..9) {
+            for(j in 0..9) {
+                mString += " | " + gameBoard[i][j].toString()
+                if(j==9) {
+                    mString += " |\n"
+                    Log.d("GameBoard: ", mString.toString())
+                }
+            }
         }
     }
 
+    private fun placeFiguresOnGameBoard(color: Int) {
+        if(color == 0) {
+            for (i in 0..3) {
+                for (j in 0..9) {
+                    var item: Figure = redFiguresOnBoard.first()
+                    redFiguresOnBoard.removeAt(0)
+                    gameBoard[i][j] = item.value
+                }
+            }
+        } else {
+            for (i in 6..9) {
+                for (j in 0..9) {
+                    var item: Figure = blueFiguresOnBoard.first()
+                    blueFiguresOnBoard.removeAt(0)
+                    gameBoard[i][j] = item.value
+                }
+            }
+        }
+    }
+
+    private fun shufflePieces(figuresOnBoard: ArrayList<Figure>) {
+        Collections.shuffle(figuresOnBoard);
+    }
     private fun initalizePieces() {
         //red pieces
-        figuresOnBoard.add(Flag(StaticFields.RED, 100, 1))
-        figuresOnBoard.add(Bomb(StaticFields.RED, 11, 6))
-        figuresOnBoard.add(Marshal(StaticFields.RED, 10, 1))
-        figuresOnBoard.add(General(StaticFields.RED, 9, 1))
-        figuresOnBoard.add(Colonel(StaticFields.RED, 8, 2))
-        figuresOnBoard.add(Major(StaticFields.RED, 7, 3))
-        figuresOnBoard.add(Captain(StaticFields.RED, 6, 4))
-        figuresOnBoard.add(Lieutenant(StaticFields.RED, 5, 4))
-        figuresOnBoard.add(Sergeant(StaticFields.RED, 4, 4))
-        figuresOnBoard.add(Minor(StaticFields.RED, 3, 5))
-        figuresOnBoard.add(Scout(StaticFields.RED, 2, 8))
-        figuresOnBoard.add(Spy(StaticFields.RED, 1, 1))
+        redFiguresOnBoard.add(Flag(StaticFields.RED, 100, 1))
+
+        redFiguresOnBoard.add(Bomb(StaticFields.RED, 11, 1))
+        redFiguresOnBoard.add(Bomb(StaticFields.RED, 11, 2))
+        redFiguresOnBoard.add(Bomb(StaticFields.RED, 11, 3))
+        redFiguresOnBoard.add(Bomb(StaticFields.RED, 11, 4))
+        redFiguresOnBoard.add(Bomb(StaticFields.RED, 11, 5))
+        redFiguresOnBoard.add(Bomb(StaticFields.RED, 11, 6))
+
+        redFiguresOnBoard.add(Marshal(StaticFields.RED, 10, 1))
+
+        redFiguresOnBoard.add(General(StaticFields.RED, 9, 1))
+
+        redFiguresOnBoard.add(Colonel(StaticFields.RED, 8, 1))
+        redFiguresOnBoard.add(Colonel(StaticFields.RED, 8, 2))
+
+        redFiguresOnBoard.add(Major(StaticFields.RED, 7, 1))
+        redFiguresOnBoard.add(Major(StaticFields.RED, 7, 2))
+        redFiguresOnBoard.add(Major(StaticFields.RED, 7, 3))
+
+        redFiguresOnBoard.add(Captain(StaticFields.RED, 6, 1))
+        redFiguresOnBoard.add(Captain(StaticFields.RED, 6, 2))
+        redFiguresOnBoard.add(Captain(StaticFields.RED, 6, 3))
+        redFiguresOnBoard.add(Captain(StaticFields.RED, 6, 4))
+
+        redFiguresOnBoard.add(Lieutenant(StaticFields.RED, 5, 1))
+        redFiguresOnBoard.add(Lieutenant(StaticFields.RED, 5, 2))
+        redFiguresOnBoard.add(Lieutenant(StaticFields.RED, 5, 3))
+        redFiguresOnBoard.add(Lieutenant(StaticFields.RED, 5, 4))
+
+        redFiguresOnBoard.add(Sergeant(StaticFields.RED, 4, 1))
+        redFiguresOnBoard.add(Sergeant(StaticFields.RED, 4, 2))
+        redFiguresOnBoard.add(Sergeant(StaticFields.RED, 4, 3))
+        redFiguresOnBoard.add(Sergeant(StaticFields.RED, 4, 4))
+
+        redFiguresOnBoard.add(Minor(StaticFields.RED, 3, 1))
+        redFiguresOnBoard.add(Minor(StaticFields.RED, 3, 2))
+        redFiguresOnBoard.add(Minor(StaticFields.RED, 3, 3))
+        redFiguresOnBoard.add(Minor(StaticFields.RED, 3, 4))
+        redFiguresOnBoard.add(Minor(StaticFields.RED, 3, 5))
+
+        redFiguresOnBoard.add(Scout(StaticFields.RED, 2, 1))
+        redFiguresOnBoard.add(Scout(StaticFields.RED, 2, 2))
+        redFiguresOnBoard.add(Scout(StaticFields.RED, 2, 3))
+        redFiguresOnBoard.add(Scout(StaticFields.RED, 2, 4))
+        redFiguresOnBoard.add(Scout(StaticFields.RED, 2, 5))
+        redFiguresOnBoard.add(Scout(StaticFields.RED, 2, 6))
+        redFiguresOnBoard.add(Scout(StaticFields.RED, 2, 7))
+        redFiguresOnBoard.add(Scout(StaticFields.RED, 2, 8))
+
+        redFiguresOnBoard.add(Spy(StaticFields.RED, 1, 1))
 
         //blue pieces
-        figuresOnBoard.add(Flag(StaticFields.BLUE, 100, 1))
-        figuresOnBoard.add(Bomb(StaticFields.BLUE, 11, 6))
-        figuresOnBoard.add(Marshal(StaticFields.BLUE, 10, 1))
-        figuresOnBoard.add(General(StaticFields.BLUE, 9, 1))
-        figuresOnBoard.add(Colonel(StaticFields.BLUE, 8, 2))
-        figuresOnBoard.add(Major(StaticFields.BLUE, 7, 3))
-        figuresOnBoard.add(Captain(StaticFields.BLUE, 6, 4))
-        figuresOnBoard.add(Lieutenant(StaticFields.BLUE, 5, 4))
-        figuresOnBoard.add(Sergeant(StaticFields.BLUE, 4, 4))
-        figuresOnBoard.add(Minor(StaticFields.BLUE, 3, 5))
-        figuresOnBoard.add(Scout(StaticFields.BLUE, 2, 8))
-        figuresOnBoard.add(Spy(StaticFields.BLUE, 1, 1))
+        blueFiguresOnBoard.add(Flag(StaticFields.BLUE, 100, 1))
+
+        blueFiguresOnBoard.add(Bomb(StaticFields.BLUE, 11, 1))
+        blueFiguresOnBoard.add(Bomb(StaticFields.BLUE, 11, 2))
+        blueFiguresOnBoard.add(Bomb(StaticFields.BLUE, 11, 3))
+        blueFiguresOnBoard.add(Bomb(StaticFields.BLUE, 11, 4))
+        blueFiguresOnBoard.add(Bomb(StaticFields.BLUE, 11, 5))
+        blueFiguresOnBoard.add(Bomb(StaticFields.BLUE, 11, 6))
+
+        blueFiguresOnBoard.add(Marshal(StaticFields.BLUE, 10, 1))
+
+        blueFiguresOnBoard.add(General(StaticFields.BLUE, 9, 1))
+
+        blueFiguresOnBoard.add(Colonel(StaticFields.BLUE, 8, 1))
+        blueFiguresOnBoard.add(Colonel(StaticFields.BLUE, 8, 2))
+
+        blueFiguresOnBoard.add(Major(StaticFields.BLUE, 7, 1))
+        blueFiguresOnBoard.add(Major(StaticFields.BLUE, 7, 2))
+        blueFiguresOnBoard.add(Major(StaticFields.BLUE, 7, 3))
+
+        blueFiguresOnBoard.add(Captain(StaticFields.BLUE, 6, 1))
+        blueFiguresOnBoard.add(Captain(StaticFields.BLUE, 6, 2))
+        blueFiguresOnBoard.add(Captain(StaticFields.BLUE, 6, 3))
+        blueFiguresOnBoard.add(Captain(StaticFields.BLUE, 6, 4))
+
+        blueFiguresOnBoard.add(Lieutenant(StaticFields.BLUE, 5, 1))
+        blueFiguresOnBoard.add(Lieutenant(StaticFields.BLUE, 5, 2))
+        blueFiguresOnBoard.add(Lieutenant(StaticFields.BLUE, 5, 3))
+        blueFiguresOnBoard.add(Lieutenant(StaticFields.BLUE, 5, 4))
+
+        blueFiguresOnBoard.add(Sergeant(StaticFields.BLUE, 4, 1))
+        blueFiguresOnBoard.add(Sergeant(StaticFields.BLUE, 4, 2))
+        blueFiguresOnBoard.add(Sergeant(StaticFields.BLUE, 4, 3))
+        blueFiguresOnBoard.add(Sergeant(StaticFields.BLUE, 4, 4))
+
+        blueFiguresOnBoard.add(Minor(StaticFields.BLUE, 3, 1))
+        blueFiguresOnBoard.add(Minor(StaticFields.BLUE, 3, 2))
+        blueFiguresOnBoard.add(Minor(StaticFields.BLUE, 3, 3))
+        blueFiguresOnBoard.add(Minor(StaticFields.BLUE, 3, 4))
+        blueFiguresOnBoard.add(Minor(StaticFields.BLUE, 3, 5))
+
+        blueFiguresOnBoard.add(Scout(StaticFields.BLUE, 2, 1))
+        blueFiguresOnBoard.add(Scout(StaticFields.BLUE, 2, 2))
+        blueFiguresOnBoard.add(Scout(StaticFields.BLUE, 2, 3))
+        blueFiguresOnBoard.add(Scout(StaticFields.BLUE, 2, 4))
+        blueFiguresOnBoard.add(Scout(StaticFields.BLUE, 2, 5))
+        blueFiguresOnBoard.add(Scout(StaticFields.BLUE, 2, 6))
+        blueFiguresOnBoard.add(Scout(StaticFields.BLUE, 2, 7))
+        blueFiguresOnBoard.add(Scout(StaticFields.BLUE, 2, 8))
+
+        blueFiguresOnBoard.add(Spy(StaticFields.BLUE, 1, 1))
     }
 }
--- app/src/main/java/at/co/netconsulting/general/StaticFields.kt
+++ app/src/main/java/at/co/netconsulting/general/StaticFields.kt
@@ -2,7 +2,7 @@ package at.co.netconsulting.general
 
 class StaticFields {
     companion object {
-        const val RED = 0;
-        const val BLUE = 1;
+        const val RED = 1;
+        const val BLUE = 0;
     }
 }
